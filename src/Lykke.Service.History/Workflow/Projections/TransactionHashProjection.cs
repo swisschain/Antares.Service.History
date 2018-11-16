482d5f6ca10c0424e4df6560e39efd0e009749ef
AleksandrBelousov <tortyt1@gmail.com>
2018-11-16 16:57:05 +0400

    changed exc throwing to log writing

--- src/Lykke.Service.History/Workflow/Projections/TransactionHashProjection.cs
+++ src/Lykke.Service.History/Workflow/Projections/TransactionHashProjection.cs
@@ -28,8 +28,15 @@ namespace Lykke.Service.History.Workflow.Projections
         public async Task<CommandHandlingResult> Handle(CashinCompletedEvent @event)
         {
             if (!await _historyRecordsRepository.UpdateBlockchainHashAsync(@event.OperationId, @event.TxHash))
-                throw new InvalidOperationException($"Transaction hash was not set. " +
-                                                    $"OperationId: {@event.OperationId}, TxHash: {@event.TxHash}");
+            {
+                _logger.Warning($"Transaction hash was not set", context: new
+                {
+                    id = @event.OperationId,
+                    hash = @event.TxHash
+                });
+
+                return CommandHandlingResult.Fail(TimeSpan.FromMinutes(1));
+            }
 
             return CommandHandlingResult.Ok();
         }
@@ -42,8 +49,15 @@ namespace Lykke.Service.History.Workflow.Projections
         public async Task<CommandHandlingResult> Handle(CashoutCompletedEvent @event)
         {
             if (!await _historyRecordsRepository.UpdateBlockchainHashAsync(@event.OperationId, @event.TxHash))
-                throw new InvalidOperationException($"Transaction hash was not set. " +
-                                                    $"OperationId: {@event.OperationId}, TxHash: {@event.TxHash}");
+            {
+                _logger.Warning($"Transaction hash was not set", context: new
+                {
+                    id = @event.OperationId,
+                    hash = @event.TxHash
+                });
+
+                return CommandHandlingResult.Fail(TimeSpan.FromMinutes(1));
+            }
 
             return CommandHandlingResult.Ok();
         }
@@ -57,8 +71,15 @@ namespace Lykke.Service.History.Workflow.Projections
             Job.BlockchainCashinDetector.Contract.Events.CashinCompletedEvent @event)
         {
             if (!await _historyRecordsRepository.UpdateBlockchainHashAsync(@event.OperationId, @event.TransactionHash))
-                throw new InvalidOperationException($"Transaction hash was not set. " +
-                                                    $"OperationId: {@event.OperationId}, TxHash: {@event.TransactionHash}");
+            {
+                _logger.Warning($"Transaction hash was not set", context: new
+                {
+                    id = @event.OperationId,
+                    hash = @event.TransactionHash
+                });
+
+                return CommandHandlingResult.Fail(TimeSpan.FromMinutes(1));
+            }
 
             return CommandHandlingResult.Ok();
         }
@@ -72,8 +93,15 @@ namespace Lykke.Service.History.Workflow.Projections
             Job.BlockchainCashoutProcessor.Contract.Events.CashoutCompletedEvent @event)
         {
             if (!await _historyRecordsRepository.UpdateBlockchainHashAsync(@event.OperationId, @event.TransactionHash))
-                throw new InvalidOperationException($"Transaction hash was not set. " +
-                                                    $"OperationId: {@event.OperationId}, TxHash: {@event.TransactionHash}");
+            {
+                _logger.Warning($"Transaction hash was not set", context: new
+                {
+                    id = @event.OperationId,
+                    hash = @event.TransactionHash
+                });
+
+                return CommandHandlingResult.Fail(TimeSpan.FromMinutes(1));
+            }
 
             return CommandHandlingResult.Ok();
         }
@@ -88,14 +116,24 @@ namespace Lykke.Service.History.Workflow.Projections
         {
             if (@event.Cashouts == null || @event.Cashouts.Length == 0)
             {
-               throw new NotImplementedException($"BIL batched cashout event, BatchId {@event.BatchId}");
+                _logger.Warning($"BIL batched cashout event is empty, BatchId {@event.BatchId}", context: @event);
+
+                return CommandHandlingResult.Ok();
             }
 
             foreach (var cashout in @event.Cashouts)
             {
-                if (!await _historyRecordsRepository.UpdateBlockchainHashAsync(cashout.OperationId, @event.TransactionHash))
-                    throw new InvalidOperationException($"Transaction hash was not set. " +
-                                                        $"OperationId: {cashout.OperationId}, TxHash: {@event.TransactionHash}");
+                if (!await _historyRecordsRepository.UpdateBlockchainHashAsync(cashout.OperationId,
+                    @event.TransactionHash))
+                {
+                    _logger.Warning($"Transaction hash was not set, BIL cashout", context: new
+                    {
+                        id = cashout.OperationId,
+                        hash = @event.TransactionHash
+                    });
+
+                    return CommandHandlingResult.Fail(TimeSpan.FromMinutes(1));
+                }
             }
 
             return CommandHandlingResult.Ok();
@@ -111,13 +149,31 @@ namespace Lykke.Service.History.Workflow.Projections
         {
             if (!await _historyRecordsRepository.UpdateBlockchainHashAsync(@event.OperationId,
                 _crossClientTransactionHashSubstituition))
-                throw new InvalidOperationException($"Transaction hash was not set. " +
-                                                    $"OperationId: {@event.OperationId}, TxHash: {_crossClientTransactionHashSubstituition}");
+            {
+                _logger.Warning($"Transaction hash was not set. " +
+                                $"OperationId: {@event.OperationId}, " +
+                                $"TxHash: {_crossClientTransactionHashSubstituition}", context: new
+                                {
+                                    id = @event.OperationId,
+                                    hash = _crossClientTransactionHashSubstituition
+                                });
+
+                return CommandHandlingResult.Fail(TimeSpan.FromMinutes(1));
+            }
 
             if (!await _historyRecordsRepository.UpdateBlockchainHashAsync(@event.CashinOperationId,
                 _crossClientTransactionHashSubstituition))
-                throw new InvalidOperationException($"Transaction hash was not set. " +
-                                                    $"OperationId: {@event.OperationId}, TxHash: {_crossClientTransactionHashSubstituition}");
+            {
+                _logger.Warning($"Transaction cashin hash was not set. " +
+                                $"OperationId: {@event.OperationId}, " +
+                                $"TxHash: {_crossClientTransactionHashSubstituition}", context: new
+                {
+                    id = @event.OperationId,
+                    hash = _crossClientTransactionHashSubstituition
+                });
+
+                return CommandHandlingResult.Fail(TimeSpan.FromMinutes(1));
+            }
 
             return CommandHandlingResult.Ok();
         }
